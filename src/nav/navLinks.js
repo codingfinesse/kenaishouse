c6c12489d6190f39d01a07f9bb4e23175634e1ea
Darren Tran <darrenptran@gmail.com>
2023-01-10 09:18:01 -0800

    ic

--- src/nav/navLinks.js
+++
@@ -1,5 +0,0 @@
-export const navLinks = [
-	{ navLinkId: 'Home', scrollToId: 'homeContainer' },
-	{ navLinkId: 'About', scrollToId: 'aboutContainer' },
-	{ navLinkId: 'Contact', scrollToId: 'contactContainer' },
-];
